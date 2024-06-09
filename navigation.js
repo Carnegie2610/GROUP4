9235d9421834eb5c287dbf77b68c51054a046a3e
Carnegie2610 <ronsard44djoumessi@gmail.com>
2024-06-09 16:21:11 +0100

    fix : routing options configured

--- navigation.js
+++ navigation.js
@@ -2,25 +2,20 @@ import * as React from 'react';
 import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-    </View>
-  );
-}
-
+import HomeScreen from './Screen/HomeScreen';
+import LoginPage from './Screen/LoginPage';
 const Stack = createNativeStackNavigator();
 
-function App() {
+function Navigation() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="LoginPage" component={LoginPage} />
+
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
-export default App;
+export default Navigation;
