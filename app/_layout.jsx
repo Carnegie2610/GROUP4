73f8fb5b301918c455fe9d8485b1e8be25dc03e5
Carnegie2610 <ronsard44djoumessi@gmail.com>
2024-07-04 05:28:04 +0100

    feat(file): now all the tabs are working but an extra tab has to be removed

---
+++ app/_layout.jsx
@@ -0,0 +1,31 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React, { useCallback, useEffect } from 'react'
+import { SplashScreen, Stack } from 'expo-router'
+import { useFonts } from 'expo-font'
+
+SplashScreen.preventAutoHideAsync();
+
+const RootLayout = () => 
+  {
+    const [fontsLoaded, error] = useFonts({
+      'PlaywriteDEGrund-VariableFont_wght': require('../assets/fonts/PlaywriteDEGrund-VariableFont_wght.ttf')
+    })
+   
+    useEffect(()=>{
+      if(error) throw error 
+      if (fontsLoaded) SplashScreen.hideAsync()
+
+    },[fontsLoaded,error])
+
+      if(!fontsLoaded && !error) return null 
+    return (
+      <>
+          <Stack>
+            <Stack.Screen name='index' options={{headerShown : false }}/>
+          </Stack> 
+      </>
+      
+    )
+  }
+
+export default RootLayout
