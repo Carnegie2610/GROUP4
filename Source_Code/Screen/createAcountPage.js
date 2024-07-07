5b190de7c42555361277dadab8802a2252229c88
Carnegie2610 <ronsard44djoumessi@gmail.com>
2024-07-07 16:11:46 +0100

    feat(filename): source code folder created where the code is available

---
+++ Source_Code/Screen/createAcountPage.js
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { TextInputComponent } from 'react-native';
+import TextInput from '../components/textInputComponent';
+const CreateAccount = () => {
+  return (
+      <View>
+      <TextInputComponent/>
+
+      </View>
+  );
+};
+
+export default CreateAccount;
